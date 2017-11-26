a7ca87655eb5fc3e1e4274da3387b9091814261c
Peeter <peeter.pihus@khk.ee>
2017-11-26 14:17:11 +0200

    Added data request

--- index.php
+++ index.php
@@ -3,13 +3,6 @@ ob_start();
 session_start();
 require_once 'dbconnect.php';
 require_once 'funktsioon.php';
-//if (!isset($_SESSION['userName'])) {
-//    header("Location: index.php");
-//    exit;
-//}
-//$res = $conn->query("set names utf8");
-//$res = $conn->query("SELECT * FROM users WHERE idUser=" . $_SESSION['userName']);
-//$userRow = mysqli_fetch_array($res, MYSQLI_ASSOC);
 $rows_raw = $conn->query("SELECT * FROM register");
 foreach ($rows_raw as $row) {
     $name=getname($row['isikukood']);
@@ -39,33 +32,12 @@ foreach ($rows_raw as $row) {
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
-<!--            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"-->
-<!--                    aria-expanded="false" aria-controls="navbar">-->
-<!--                <span class="sr-only">Toggle navigation</span>-->
-<!--                <span class="icon-bar"></span>-->
-<!--                <span class="icon-bar"></span>-->
-<!--                <span class="icon-bar"></span>-->
-<!--            </button>-->
-<!--            <a class="navbar-brand" href="index.php">Äriregister</a>-->
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="#">Register</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-
-                <!--                <li class="dropdown">-->
-                <!--                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"-->
-                <!--                       aria-expanded="false">-->
-                <!--                        <span-->
-                <!--                            class="glyphicon glyphicon-user"></span>&nbsp;Logged-->
-                <!--                        in: --><?php //echo $userRow['name']; ?><!-- --><?php //echo $userRow['surname']; ?>
-                <!--                        &nbsp;<span class="caret"></span></a>-->
-                <!--                    <ul class="dropdown-menu">-->
-                <!--                        <li><a href="index.php"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Logout</a>-->
-                <!--                        </li>-->
-                <!--                    </ul>-->
-                <!--                </li>-->
             </ul>
         </div>
         <table>
