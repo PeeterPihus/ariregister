26c75a5dfe1b0164fccee78c09351a9c16cee69c
Peeter <peeter.pihus@khk.ee>
2017-12-08 17:38:23 +0200

    Added data request

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ foreach ($rows_raw as $row) {
 }
 foreach ($rows_raw as $row) {
     $name=getname($row['isikukood']);
-    $encoded = array($row['idReg'], $name['person_first_name'], $name['person_last_name'], $row['company'], $row['aadress'], $row['kapital'], $row['internetAadress'], $row['staatus'], $row['isikukood']);
+    $encoded = array($row['idReg'], $name['person_first_name'], 'surnameReg', 'company');
     echo json_encode($encoded);
 }
 ?>
